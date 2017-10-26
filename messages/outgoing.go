5ac2d013c4a4097f3d35708be00dade65095a082
Sergio Vera Castellano <severa@magento.com>
2017-10-26 16:49:10 +0200

    Reorganised file

--- messages/outgoing.go
+++ messages/outgoing.go
@@ -20,17 +20,13 @@ const (
 	ReasonPlayerQuitted             = "qui"
 )
 
-// Types for the messages sent to the clients.
-const (
-	TypeClientOut        = "out"
-	TypeRoomsList        = "rms"
-	TypeCurrentPlayers   = "pls"
-	TypeError            = "err"
-	TypeJoinedRoom       = "joi"
-	TypeUpdateGameStatus = "upd"
-	TypeGameStarted      = "gst"
-)
+// TypeUpdateGameStatus defines the value that update game status
+// messages must have in the Type field.
+const TypeUpdateGameStatus = "upd"
 
+// TypeClientOut defines the value that client out
+// messages must have in the Type field.
+//
 // ClientOut is sent to a client when he/she is expelled from a room.
 // The following is a ClientOut message example:
 //   {
@@ -39,10 +35,17 @@ const (
 //       "rea": "tim"
 //     }
 //   }
+const TypeClientOut = "out"
+
+// ClientOut defines the needed parameters for a client out
+// message.
 type ClientOut struct {
 	Reason string `json:"rea"`
 }
 
+// TypeRoomsList defines the value that rooms list
+// messages must have in the Type field.
+//
 // RoomsList is sent to all clients when a new room is created.
 // It contains all available rooms (rooms which haven't started a game yet).
 // The following is a RoomsList message example:
@@ -52,10 +55,17 @@ type ClientOut struct {
 //       "val": ["VWXYZ", "ABCDE"]
 //     }
 //   }
+const TypeRoomsList = "rms"
+
+// RoomsList defines the needed parameters for a rooms list
+// message.
 type RoomsList struct {
 	Values []string `json:"val"`
 }
 
+// TypeCurrentPlayers defines the value that current players
+// messages must have in the Type field.
+//
 // CurrentPlayers is sent to all clients in a room when a player enters or leaves the room.
 // The following is a CurrentPlayers message example:
 //   {
@@ -68,6 +78,10 @@ type RoomsList struct {
 //       }
 //     }
 //   }
+const TypeCurrentPlayers = "pls"
+
+// CurrentPlayers defines the needed parameters for a current players
+// message.
 type CurrentPlayers struct {
 	Values map[string]PlayerData `json:"val"`
 }
@@ -78,6 +92,9 @@ type PlayerData struct {
 	Name string `json:"nam"`
 }
 
+// TypeError defines the value that error
+// messages must have in the Type field.
+//
 // Error is a message sent to a specific player
 // when he/she does an action that leads to an error.
 // The following is a Error message example:
@@ -87,10 +104,17 @@ type PlayerData struct {
 //       "des": "Whatever"
 //     }
 //   }
+const TypeError = "err"
+
+// Error defines the needed parameters for an error
+// message.
 type Error struct {
 	Description string `json:"des"`
 }
 
+// TypeJoinedRoom defines the value that joined room
+// messages must have in the Type field.
+//
 // JoinedRoom is a message sent to a specific player
 // when he/she joins to a room.
 // The following is a JoinedRoom message example:
@@ -102,6 +126,10 @@ type Error struct {
 //       "own": false
 //     }
 //   }
+const TypeJoinedRoom = "joi"
+
+// JoinedRoom defines the needed parameters for a joined room
+// message.
 type JoinedRoom struct {
 	ClientNumber int    `json:"num"`
 	ID           string `json:"id"`
@@ -109,6 +137,9 @@ type JoinedRoom struct {
 	Owner bool `json:"own"`
 }
 
+// TypeGameStarted defines the value that game started
+// messages must have in the Type field.
+//
 // GameStarted is a message sent to all players
 // when a game starts.
 // The following is a GameStarted message example:
@@ -121,6 +152,10 @@ type JoinedRoom struct {
 //       }
 //     }
 //   }
+const TypeGameStarted = "gst"
+
+// GameStarted defines the needed parameters for a game started
+// message.
 type GameStarted struct {
 	PlayerTimeOut  time.Duration   `json:"pto"`
 	GameParameters json.RawMessage `json:"gpa"`
