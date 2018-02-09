44e8ea8db9e8161896b241eb26a7659f36028b93
Sergio Vera Castellano <severa@magento.com>
2018-02-09 15:26:47 +0100

    Improved parameters naming

--- api/driver.go
+++ api/driver.go
@@ -7,12 +7,12 @@ import (
 // Driver is an interface that defines the minimum set of functions needed
 // to implement a game driver which can be used within a hub instance
 type Driver interface {
-	Execute(clientName string, t string, content json.RawMessage) error
+	Execute(clientName string, messageType string, content json.RawMessage) error
 	CurrentPlayersNumbers() ([]int, error)
 	Status(playerNumber int) (interface{}, error)
 	RemovePlayer(number int) error
 	CreateAI(params interface{}) (AI, error)
-	StartGame(map[int]string) error
+	StartGame(clientNames map[int]string) error
 	GameStarted() bool
 	IsGameOver() bool
 	Name() string
